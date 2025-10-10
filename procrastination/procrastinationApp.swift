987ffceebbe7e5dd2fdbb40f9fe28d3946bd37f3
Iris <112306032@nccu.edu.tw>
2025-10-10 21:23:31 +0800

    Initial Commit

---
+++ procrastination/procrastinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  procrastinationApp.swift
+//  procrastination
+//
+//  Created by Iris Tsou on 2025/10/10.
+//
+
+import SwiftUI
+
+@main
+struct procrastinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
